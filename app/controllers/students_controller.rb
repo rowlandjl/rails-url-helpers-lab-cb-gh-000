cee9f90dc8408585b548bc7e04282fab611c648b
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-12 01:16:26 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,7 +14,7 @@ class StudentsController < ApplicationController
     else
       @student.update(active: true)
     end
-    redirect_to students_path(@student)
+    redirect_to student_path(@student)
   end
 
   private
